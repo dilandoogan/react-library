7b7d31ef7ef9eeb0101dd06470344215acbbc692
dilan.dogan2 <dilan.dogan2@trendyol.com>
2023-11-26 19:08:27 +0300

    feat: add disabled feature to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import "./Button.scss";
 
 export interface ButtonProps {
   label: string;
+  disabled?: boolean;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+const Button = ({label,disabled=false}: ButtonProps) => {
+  return <button disabled={disabled}>{label}</button>;
 };
 
 export default Button;
